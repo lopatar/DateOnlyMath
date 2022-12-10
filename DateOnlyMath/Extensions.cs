068320585c7b7bb9a2870c9d0690de248b43cf37
Jiří Lopatář <github@lopatar.me>
2022-12-10 12:35:41 +0100

    Initial upload

---
+++ DateOnlyMath/Extensions.cs
@@ -0,0 +1,34 @@
+﻿namespace DateOnlyMath;
+
+public static class Extensions
+{
+    public static void Add(ref this DateOnly date, DateOnly dateToAdd)
+    {
+        date = date.AddDays(dateToAdd.DayNumber);
+    }
+
+    public static void Subtract(ref this DateOnly date, DateOnly dateToSubtract)
+    {
+        date = new DateOnly().AddDays(date.DayNumber - dateToSubtract.DayNumber);
+    }
+
+    public static void Multiply(ref this DateOnly date, DateOnly dateToMultiply)
+    {
+        date = new DateOnly().AddDays(date.DayNumber * dateToMultiply.DayNumber);
+    }
+
+    public static void Divide(ref this DateOnly date, DateOnly dateToDividedBy) 
+    {
+        date = new DateOnly().AddDays(date.DayNumber / dateToDividedBy.DayNumber);
+    }
+
+    public static bool BiggerThan(this DateOnly date, DateOnly dateToCompare)
+    {
+        return date.CompareTo(dateToCompare) > 0;
+    }
+
+    public static bool LowerThan(this DateOnly date, DateOnly dateToCompare)
+    {
+        return date.CompareTo(dateToCompare) < 0;
+    }
+}
